f8cda5277eca3f23d4513c14e66600114aa08f27
Liza Antonova <Elizaveta.Antonova@urfu.me>
2023-05-19 22:08:39 +0500

    кодстайл

--- tests/test_connection_arp.py
+++ tests/test_connection_arp.py
@@ -7,6 +7,7 @@ class TestConnArp:
     def test_plug_arp_host(self):
         assert send_frame('192.168.0.8', 1, 2)[0] \
                == 'Connection to 192.168.0.8: layer=data link, arp_seq=3'
-        assert send_frame('192.168.254.254', 1, 2)[0] == f'Connection timed out :('
+        assert send_frame('192.168.254.254', 1, 2)[0] == \
+               f'Connection timed out :('
         with pytest.raises(SystemExit):
             send_frame('192.168.256.256', 1, 2)
